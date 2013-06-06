2312757795a66d4b49bd1a082561eb2875feda87
Judd Blair <juddblair@gmail.com>
2013-06-05 17:53:07 -0700

    added middleman sync

---
+++ euclid_crypt.rb
@@ -0,0 +1,18 @@
+# Based on https://gist.github.com/728456
+
+module EuclidCrypt
+  require 'base64'
+
+  EUCLID_SALT = "JlNO5MO40fLWEiZdgzL"
+  
+  def self.encrypt(salt, str) 
+    cipher = Gibberish::AES.new(salt)
+    Base64.urlsafe_encode64(cipher.enc(str, {:binary => true}))
+  end
+
+  def self.decrypt(salt, str)
+    cipher = Gibberish::AES.new(salt)
+    cipher.dec(Base64.urlsafe_decode64(str), {:binary => true})
+  end
+  
+end
