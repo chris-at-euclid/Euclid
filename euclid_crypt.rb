3f1251b2b6fbd58f097b180a17140f05dcc0cae1
Emma Cunningham <the.cunning.ham@gmail.com>
2013-12-18 16:43:39 -0800

    pushing to the right branch now

--- euclid_crypt.rb
+++ euclid_crypt.rb
@@ -4,15 +4,15 @@ module EuclidCrypt
   require 'base64'
 
   EUCLID_SALT = "JlNO5MO40fLWEiZdgzL"
-  
-  def self.encrypt(salt, str) 
+
+  def self.encrypt(salt, str)
     cipher = Gibberish::AES.new(salt)
     Base64.urlsafe_encode64(cipher.enc(str, {:binary => true}))
   end
 
   def self.decrypt(salt, str)
     cipher = Gibberish::AES.new(salt)
-    cipher.dec(Base64.urlsafe_decode64(str), {:binary => true})
+    #cipher.dec(Base64.urlsafe_decode64(str), {:binary => true})
   end
-  
+
 end
